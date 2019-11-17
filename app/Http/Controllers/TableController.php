a249d40e5ea70d971a18de172f6a9bcb82938e0f
jakubfilistynski <j.filistynski@bonasoft.pl>
2019-11-17 22:31:26 +0100

    table controller

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Table;
+use App\Models\User;
+use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\JWT;
 
 class TableController extends Controller
 {
+
     /**
-     * Display a listing of the resource.
+     * Show the application dashboard.
      *
-     * @return \Illuminate\Http\Response
+     * @return Renderable
      */
     public function index()
     {
@@ -18,24 +24,15 @@ class TableController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Show the form for editing the specified resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param  int  $id
+     * @return Response
      */
-    public function create()
+    public function edit($id)
     {
-        return view('tables/create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        $table = Table::find($id);
+        return view('tables/edit', compact(['table', 'id']));
     }
 
     /**
@@ -44,44 +41,20 @@ class TableController extends Controller
      * @param  int  $id
      * @return Response
      */
-    public function showWaiter($id)
+    public function show($id)
     {
-        return view('tables/waiterShow',compact(["id"]));
+        $table = Table::find($id);
+        return view('tables/show', compact(['table', 'id']));
     }
 
 
-
     /**
-     * Show the form for editing the specified resource.
+     * Show the application dashboard.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Renderable
      */
-    public function edit($id)
+    public function waiterIndex()
     {
-        return view('tables/edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return view('tables/waiterIndex');
     }
 }
