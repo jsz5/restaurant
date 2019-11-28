2ef3edce299992c1d1222a4a5b1d3bdc8e5dcc50
Jakub Pyka <pykajakubny@gmail.com>
2019-11-28 18:11:18 +0100

    maile

--- app/Mails/RegistrationMail.php
+++ app/Mails/RegistrationMail.php
@@ -5,6 +5,8 @@ namespace App\Mails;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 
@@ -15,31 +17,53 @@ class RegistrationMail extends Mailable
     public $sendToMail;
     public $password;
     public $link;
+    private $role;
     private const SUBJECT="Rejestracja w systemie restauracji \"W-17 wydział smaków\"";
 
-    public function __construct(string $password, string $login)
+    /**
+     * RegistrationMail constructor.
+     * @param string $password
+     * @param string $login
+     * @param string $role
+     * @codeCoverageIgnore
+     */
+    public function __construct(string $password, string $login, string $role)
     {
         $this->sendToMail=$login;
         $this->password=$password;
         $this->link=route('login');
+        $this->role=$role;
     }
 
     /**
      * Build the message.
-     *
+     * @codeCoverageIgnore
      * @return $this
      */
     public function build()
     {
-        return $this->view('mails.registration') ->subject(self::SUBJECT);
+        return $this->checkRole();
     }
 
     /**
+     * @return RegistrationMail
+     * different mail for customer and worker
+     * @codeCoverageIgnore
+     */
+    private function checkRole()
+    {
+        if($this->role=='customer'){
+            return $this->view('mails.registrationCustomer') ->subject(self::SUBJECT);
+        }
+        return $this->view('mails.registration') ->subject(self::SUBJECT);
+    }
+    /**
      * sends mail to customer
+     * @codeCoverageIgnore
      */
     public function sendMail()
     {
+        Log::notice("Mail registration to:" . $this->sendToMail);
         Mail::to($this->sendToMail)->queue($this);
-
     }
 }
--- app/Mails/ResetPasswordMail.php
+++ app/Mails/ResetPasswordMail.php
@@ -5,6 +5,7 @@ namespace App\Mails;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 
@@ -16,6 +17,12 @@ class ResetPasswordMail extends Mailable
     public $link;
     private const SUBJECT="Zmiana hasła w systemie restauracji \"W-17 wydział smaków\"";
 
+    /**
+     * ResetPasswordMail constructor.
+     * @param string $token
+     * @param string $email
+     * @codeCoverageIgnore
+     */
     public function __construct(string $token, string $email)
     {
         $this->sendToMail=$email;
@@ -24,7 +31,7 @@ class ResetPasswordMail extends Mailable
 
     /**
      * Build the message.
-     *
+     *  @codeCoverageIgnore
      * @return $this
      */
     public function build()
@@ -34,10 +41,11 @@ class ResetPasswordMail extends Mailable
 
     /**
      * sends mail to customer
+     * @codeCoverageIgnore
      */
     public function sendMail()
     {
+        Log::notice("Mail reset password to:" . $this->sendToMail);
         Mail::to($this->sendToMail)->queue($this);
-
     }
 }
