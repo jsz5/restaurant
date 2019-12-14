33c0ebfa9aad9307ffcee17f02e9e286df69f21e
Joanna Szołomicka <j.szolomicka@gmail.com>
2019-12-14 18:30:09 +0100

    Merge branch 'staging' into staging23
    # Conflicts:
    #	.gitignore
    #	.lando.yml
    #	package-lock.json
    #	public/js/app.js

--- app/Http/Controllers/API/ApiAuthenticateController.php
+++ app/Http/Controllers/API/ApiAuthenticateController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Services\UserService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -24,7 +25,7 @@ class ApiAuthenticateController extends Controller
             return response()->json(['error' => 'could_not_create_token'], 500);
         }
         $auth=Auth::user();
-        return response()->json(['token' => $token, 'fullName' => $auth->name." ".$auth->surname, 'email'=>$auth->email]);
+        return response()->json(['token' => $token, 'user' => (new UserService())->getUserData(Auth::user())]);
     }
 
     /**
