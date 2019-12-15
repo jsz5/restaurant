fd7c94d5d4edd17d1cd38a9870cef6d8a52d0b08
Jakub P <pykajakubny@gmail.com>
2019-12-15 14:45:40 +0100

    scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('reservation:remainder')->dailyAt('8:00');
+//        $schedule->command('reservation:remainder')->dailyAt('12:00');
+        $schedule->command('reservation:remainder')->hourly();
     }
 
     /**
